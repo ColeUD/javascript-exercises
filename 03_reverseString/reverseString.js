11533ad88fb4f8e8c65a96ecd598a13bcba5cbb0
ColeUD <jcole.ud@gmail.com>
2025-01-04 21:29:09 -0500

    pushing completed javascript exercises

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,14 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    let result = '';
+    for (let i = string.length - 1; i >= 0; i--) {
+        result += string[i];
+    }
+    return result;
 };
 
 // Do not edit below this line
 module.exports = reverseString;
+
+// looks like it turns the string into an array, and then goes backwards through 
+// the string by concatenating the character at string[i] into a new string called result
+// so hello will return olleh
