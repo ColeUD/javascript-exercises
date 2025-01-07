7e6d7ebc7d5a44bc3cb1df68f5474567c3a601df
ColeUD <jcole.ud@gmail.com>
2025-01-06 20:45:18 -0500

    done findTheOldest

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,18 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+    const currentYear = new Date().getFullYear();
+    
+    return people.reduce((oldest, person) => {
+        // Calculate age for the current person
+        const personDeathYear = person.yearOfDeath || currentYear;
+        const personAge = personDeathYear - person.yearOfBirth;
 
+        // Calculate age for the current oldest person
+        const oldestDeathYear = oldest.yearOfDeath || currentYear;
+        const oldestAge = oldestDeathYear - oldest.yearOfBirth;
+
+        // Compare ages and return the oldest
+        return personAge > oldestAge ? person : oldest;
+    });
 };
 
 // Do not edit below this line
