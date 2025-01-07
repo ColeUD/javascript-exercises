9fb37678f329cd0b2d10b3b363a336c207670a54
ColeUD <jcole.ud@gmail.com>
2025-01-06 20:29:52 -0500

    completed palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,9 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+   
+    // Remove non-alphanumeric characters and convert to lowercase
+    const cleanedStr = str.replace(/[^a-zA-Z0-9]/g, '').toLowerCase();
+    // Check if the cleaned string is equal to its reverse
+    return cleanedStr === cleanedStr.split('').reverse().join('');
 };
 
 // Do not edit below this line
